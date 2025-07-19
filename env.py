01de99bca663e8628229cba4986f93f83022f0e3
Yojda <yanthekill@gmail.com>
2025-07-19 14:38:43 -0400

    COMMMMMIT

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 PORT: int = 10000
 MAGIC: int = 0x11223344
-ID: str = ''
-PLAYER_NAME: str = ''
+ID: str = '15572bfa-64c9-11f0-af62-0242ac120002'
+PLAYER_NAME: str = 'Yannis'
 
-HOST: str = ''
+HOST: str = '10.10.10.21'
--- update.py
+++ update.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from actions import Action, create_move_action
+from actions import Action, create_move_action, create_build_barricade_action
 from server import Game
 import env
 
@@ -25,32 +25,45 @@ class Agent:
         # Get the player
         player = [index for index in range(len(players)) if players[index].name() == player_name]
 
-        if len(player) == 1:
-            player_index = player[0]
+        if len(player) == 0:
+            # If the player was not found, return no orders
+            return []
 
-            # Get the terrains of the player
-            player_terrains = [(index, terrains[index]) for index in range(len(terrains)) if
-                               terrains[index].owner_index() == player_index]
-            player_terrains_index = [terrain[0] for terrain in player_terrains]
+        player_index = player[0]
 
-            orders = []
-            for pipe in pipes:
-                # If the first terrain of a connection belongs to the player and have at least one soldier
-                if (pipe.first() in player_terrains_index and
-                        pipe.second() not in player_terrains_index and
-                        terrains[pipe.first()].number_of_soldier() > 0):
-                    # Send one soldier to the end of the connection
-                    orders.append(create_move_action(terrains[pipe.first()].id(), terrains[pipe.second()].id(), 1))
+        # Get the terrains of the player
+        player_terrains = [(index, terrains[index]) for index in range(len(terrains)) if
+                           terrains[index].owner_index() == player_index]
+        player_terrains_index = [terrain[0] for terrain in player_terrains]
+        involved_pipes = [pipe for pipe in pipes if pipe.first() in player_terrains_index or pipe.second() in player_terrains_index]
 
-                # If the second terrain of a connection belongs to the player and have at least one soldier
-                if (pipe.second() in player_terrains_index and
-                        pipe.first() not in player_terrains_index and
-                        terrains[pipe.second()].number_of_soldier() > 0):
-                    # Send one soldier to the end of the connection
-                    orders.append(create_move_action(terrains[pipe.second()].id(), terrains[pipe.first()].id(), 1))
+        print(f"{player_terrains=}, {involved_pipes=}")
 
-            # Returns the order for each terrain of the player
-            return orders
+        neighbouring_terrains = [k for k in involved_pipes if k.first() not in player_terrains_index] + [k for k in involved_pipes if k.second() not in player_terrains_index]
+        ##### ACTIONS
 
-        # Returns no orders since the player was not found
-        return []
+        orders = []
+        for pipe in pipes:
+            # If the first terrain of a connection belongs to the player and have at least one soldier
+            if (pipe.first() in player_terrains_index and
+                    pipe.second() not in player_terrains_index and
+                    terrains[pipe.first()].number_of_soldier() > 0):
+                # Send one soldier to the end of the connection
+                orders.append(create_move_action(terrains[pipe.first()].id(), terrains[pipe.second()].id(), 1))
+
+            # If the second terrain of a connection belongs to the player and have at least one soldier
+            if (pipe.second() in player_terrains_index and
+                    pipe.first() not in player_terrains_index and
+                    terrains[pipe.second()].number_of_soldier() > 0):
+                # Send one soldier to the end of the connection
+                orders.append(create_move_action(terrains[pipe.second()].id(), terrains[pipe.first()].id(), 1))
+
+        for terrain_id in player_terrains_index:
+            # If the terrain is not a factory and have at least one soldier
+            if (terrains[terrain_id].number_of_soldier() > 0):
+                # Build a factory on the terrain
+                orders.append(create_build_factory_action(terrain_id))
+
+        print(f"{orders=}")
+        # Returns the order for each terrain of the player
+        return orders
